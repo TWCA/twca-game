12260abd2a60617665f791cedea76669ec62f5ef
Wren Durbano <wren.durbano@gmail.com>
2026-02-04 16:35:22 -0700

    change palette

--- src/Assets/Shaders/PaletteSwap.hlsl
+++ src/Assets/Shaders/PaletteSwap.hlsl
@@ -13,19 +13,20 @@ void paletteSwap_float(
     out float alphaOut
     )
 {
-    color = sqrt(color);
-    float x = color.r / 8.0 + floor(frac(color.b * 8.0) * 8.0) / 8.0;
-    float y = 1 - (color.g / 8.0 + floor(color.b * 8.0) / 8.0);
+    float3 raw = sqrt(color.rgb);
+    
+    int x = int(raw.r * 64.0) + (int(raw.b * 64.0) & 7) * 64;
+    int y = 511 - int(raw.g * 64.0) - (int(raw.b * 64.0) >> 3) * 64;
     
     if (time < 0.5)
     {
-        float3 day = dayDayLUT.tex.Sample(dayDayLUT.samplerstate, float2(x, y));
-        float3 dawn = dayDawnLUT.tex.Sample(dayDawnLUT.samplerstate, float2(x, y));
+        float3 day = dayDayLUT.tex.Load(int3(x, y, 0));
+        float3 dawn = dayDawnLUT.tex.Load(int3(x, y, 0));
         colorOut = lerp(day, dawn, time * 2.0);
     } else
     {
-        float3 dawn = dayDawnLUT.tex.Sample(dayDawnLUT.samplerstate, float2(x, y));
-        float3 night = dayNightLUT.tex.Sample(dayNightLUT.samplerstate, float2(x, y));
+        float3 dawn = dayDawnLUT.tex.Load(int3(x, y, 0));
+        float3 night = dayNightLUT.tex.Load(int3(x, y, 0));
         colorOut = lerp(dawn, night, time * 2.0 - 1.0);
     }
     
