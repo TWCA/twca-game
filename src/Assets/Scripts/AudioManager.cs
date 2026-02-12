4d5118fe222783e30f7902f0632f3c11acf9ecb9
Jacob McCormick <jmmcc52@hotmail.com>
2026-02-12 04:34:05 -0700

    Added more sound effects and ambience for different time periods

--- src/Assets/Scripts/AudioManager.cs
+++ src/Assets/Scripts/AudioManager.cs
@@ -10,9 +10,12 @@ public class AudioManager : MonoBehaviour
     private static AudioManager _instance;
     public static AudioManager Instance { get { return _instance; } }
 
-    public AudioSource stepSource, insectSource, rainSource;
+    public AudioSource stepSource, insectSource, rainSource, thunderSource, owls, chickadee;
     public List<AudioClip> steps;
     public List<AudioClip> stepsWet;
+    public List<AudioClip> timeJump;
+    public List<AudioClip> InventoryClick;
+    public List<AudioClip> Notifications;
     private Vector3 previousPosition;
     private RectTransform rect;
     private bool future;
@@ -26,9 +29,9 @@ public class AudioManager : MonoBehaviour
         _instance = this;
         rect = GetComponent<RectTransform>();
         timeManager = admin.GetComponent<TimeManager>();
-        playInsects();
+        playRain();
 
-        
+
         future = timeManager.IsFuture();
         //timeManager = TimeManager.Instance;
     }
@@ -44,14 +47,25 @@ public class AudioManager : MonoBehaviour
         }
         previousPosition = rect.position;
 
-        if (future == true && !rainStarted )
+        if (future == true && !rainStarted)
         {
-            playRain();
+            playTimeForward();
+            stopRain();
+            playThunder();
+            playOwls();
+            playInsects();
+            //stopChickadee();
             rainStarted = true;
         }
         if (future == false && rainStarted)
         {
-            stopRain();
+            playTimeBackward();
+            stopThunder();
+            stopOwls();
+            stopInsect();
+            playRain();
+            
+            //playChickadee();
             rainStarted = false;
         }
     }
@@ -91,9 +105,62 @@ public class AudioManager : MonoBehaviour
     {
         rainSource.Pause();
     }
-    
+
+    public void playThunder()
+    {
+        thunderSource.loop = true;
+        //rainSource.PlayOneShot(rain);
+        thunderSource.Play();
+
+    }
+
+    public void stopThunder()
+    {
+        thunderSource.Pause();
+    }
+
     public void stopInsect()
     {
         insectSource.Pause();
+    }
+    public void playTimeForward()
+    {
+        AudioClip time = timeJump[0];
+        insectSource.PlayOneShot(time);
+    }
+    public void playTimeBackward()
+    {
+        AudioClip time = timeJump[1];
+        rainSource.PlayOneShot(time);
+    }
+
+    public void playInventory()
+    {
+        AudioClip inventory = InventoryClick[0];
+        insectSource.PlayOneShot(inventory);
+    }
+    public void playOwls()
+    {
+        owls.loop = true;
+        owls.Play();
+    }
+    public void stopOwls()
+    {
+        owls.Pause();
+    }
+
+    public void playChickadee()
+    {
+        chickadee.loop = true;
+        chickadee.Play();
+    }
+    public void stopChickadee()
+    {
+        chickadee.Pause();
+    }
+    public void playNotification()
+    {
+        AudioClip notif = Notifications[0];
+        stepSource.PlayOneShot(notif);
     }
 }
