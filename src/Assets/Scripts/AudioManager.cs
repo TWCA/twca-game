eb60ad2d9e54c5bf965e532de2b691c3cf1f83f9
Adam Mehaney <adam.mehaney@gmail.com>
2026-02-12 18:14:45 -0700

    Revert "Merge remote-tracking branch 'origin/main' into level-transitions"
    This reverts commit a37b771479a2df29d34d6b4c4f60bc483d4d128e.

--- src/Assets/Scripts/AudioManager.cs
+++ src/Assets/Scripts/AudioManager.cs
@@ -10,72 +10,43 @@ public class AudioManager : MonoBehaviour
     private static AudioManager _instance;
     public static AudioManager Instance { get { return _instance; } }
 
-    public AudioSource stepSource, insectSource, rainSource, thunderSource, owls, chickadee;
+    public AudioSource stepSource, insectSource, rainSource;
     public List<AudioClip> steps;
-    public List<AudioClip> stepsWet;
-    public List<AudioClip> timeJump;
-    public List<AudioClip> InventoryClick;
-    public List<AudioClip> Notifications;
     private Vector3 previousPosition;
     private RectTransform rect;
-    private bool rainStarted = false;
-    [SerializeField] private GameObject admin;
     
 
     void Awake()
     {
         _instance = this;
         rect = GetComponent<RectTransform>();
+        playInsects();
         playRain();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isFuture = TimeManager.Instance.IsFuture();
         
         if (rect != null && rect.position != previousPosition && !stepSource.isPlaying)
         {
-            playSteps(isFuture);
+            playSteps();
             // Debug.Log("Playing step");
         }
         previousPosition = rect.position;
-
-        if (isFuture && !rainStarted)
-        {
-            playTimeForward();
-            stopRain();
-            playThunder();
-            playOwls();
-            playInsects();
-            //stopChickadee();
-            rainStarted = true;
-        }
-        if (isFuture && rainStarted)
-        {
-            playTimeBackward();
-            stopThunder();
-            stopOwls();
-            stopInsect();
-            playRain();
-            
-            //playChickadee();
-            rainStarted = false;
-        }
+        //if (!insectSource.isPlaying)
+        //{
+        //    playInsects();
+        //}
+        //if (!rainSource.isPlaying)
+        //{
+        //    playRain();
+        //}
     }
 
-    public void playSteps(bool isFuture)
+    public void playSteps()
     {
-        AudioClip step;
-        if (isFuture)
-        {
-            step = stepsWet[Random.Range(0, stepsWet.Count)];
-        }
-        else
-        {
-            step = steps[Random.Range(0, steps.Count)];
-            
-        }
+        AudioClip step = steps[Random.Range(0, steps.Count)];
         stepSource.PlayOneShot(step);
     }
     
@@ -99,62 +70,9 @@ public class AudioManager : MonoBehaviour
     {
         rainSource.Pause();
     }
-
-    public void playThunder()
-    {
-        thunderSource.loop = true;
-        //rainSource.PlayOneShot(rain);
-        thunderSource.Play();
-
-    }
-
-    public void stopThunder()
-    {
-        thunderSource.Pause();
-    }
-
+    
     public void stopInsect()
     {
         insectSource.Pause();
-    }
-    public void playTimeForward()
-    {
-        AudioClip time = timeJump[0];
-        insectSource.PlayOneShot(time);
-    }
-    public void playTimeBackward()
-    {
-        AudioClip time = timeJump[1];
-        rainSource.PlayOneShot(time);
-    }
-
-    public void playInventory()
-    {
-        AudioClip inventory = InventoryClick[0];
-        insectSource.PlayOneShot(inventory);
-    }
-    public void playOwls()
-    {
-        owls.loop = true;
-        owls.Play();
-    }
-    public void stopOwls()
-    {
-        owls.Pause();
-    }
-
-    public void playChickadee()
-    {
-        chickadee.loop = true;
-        chickadee.Play();
-    }
-    public void stopChickadee()
-    {
-        chickadee.Pause();
-    }
-    public void playNotification()
-    {
-        AudioClip notif = Notifications[0];
-        stepSource.PlayOneShot(notif);
     }
 }
--- src/Assets/Scripts/TimePortal.cs
+++ src/Assets/Scripts/TimePortal.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TimePortal : MonoBehaviour
 {
-    public string dialogKnot;
-    
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.name == "Player")
         {
-            DialogManager.Instance.StartDialog(dialogKnot, TimeManager.Instance.ToggleTime);
+            TimeManager.Instance.ToggleTime();
         }
     }
 }
