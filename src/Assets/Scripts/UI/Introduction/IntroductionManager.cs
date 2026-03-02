63069f2306bf63808f265300aae756910689a960
Adam Mehaney <adam.mehaney@gmail.com>
2026-03-02 12:56:05 -0700

    more adjustments to intro animation/cinematic

--- src/Assets/Scripts/UI/Introduction/IntroductionManager.cs
+++ src/Assets/Scripts/UI/Introduction/IntroductionManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class IntroductionManager : MonoBehaviour
 {
+    public string MainMenuSceneName;
+    public float BeforeTime = 1;
+    public float TransitionTime = 1;
+    public float ShowTime = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +23,40 @@ public class IntroductionManager : MonoBehaviour
     }
 
     IEnumerator SceneProgression() {
-        foreach (Transform child in transform) {
+        Transform[] children = transform.Cast<Transform>().ToArray();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = children[i];
+
             GameObject childObject = child.gameObject;
             UIDocument childUIDocument = childObject.GetComponent<UIDocument>();
 
-            if (childUIDocument != null) {
-                VisualElement visualElement = childUIDocument.rootVisualElement;
-
+            if (childUIDocument != null)
+            {
                 childObject.SetActive(true);
 
-                yield return new WaitForSeconds(3);
+                VisualElement visualElement = childUIDocument.rootVisualElement;
+                VisualElement animateComponent = visualElement.Q("animatecomponent");
 
-                childObject.SetActive(false);
+                animateComponent.AddToClassList("hidden");
+
+                yield return new WaitForSeconds(BeforeTime);
+
+                animateComponent.RemoveFromClassList("hidden");
+
+                yield return new WaitForSeconds(ShowTime);
+
+                animateComponent.AddToClassList("hidden");
+
+                yield return new WaitForSeconds(TransitionTime);
+
+                if (i < transform.childCount - 1) {
+                    childObject.SetActive(false);
+                }
             }
         }
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
