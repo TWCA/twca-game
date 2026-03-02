c39448b0bd47fb0c65c5d3d8492969ab47e0d691
Wren Durbano <wren.durbano@gmail.com>
2026-03-02 13:49:26 -0700

    add folliage

---
+++ src/Assets/Scripts/Foliage.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+[ExecuteInEditMode]
+public class Foliage : MonoBehaviour
+{
+    public Sprite[] sprites;
+    
+    /*
+     * Pick a "random" sprite based on the instanceID
+     */
+    private void Awake()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        if (sprites.Length > 0)
+        {
+            int restoringSeed = Random.Range(int.MinValue, int.MaxValue);
+            
+            Random.InitState(GetInstanceID());
+            int index = Random.Range(0, sprites.Length);
+            renderer.sprite = sprites[index];
+            
+            Random.InitState(restoringSeed);
+        }
+    }
+}
