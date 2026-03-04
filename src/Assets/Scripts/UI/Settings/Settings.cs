83668439ddceebe994b021efdb7cb9bd1635331d
Adam Mehaney <adam.mehaney@gmail.com>
2026-03-04 00:41:28 -0700

    settings page improvements

--- src/Assets/Scripts/UI/Settings/Settings.cs
+++ src/Assets/Scripts/UI/Settings/Settings.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Settings : SubMenu
 {
     public Button BackButton, SaveButton;
     public Slider MasterVolumeSlider;
+    public Text MasterVolumeText;
     private SettingsLoader settingsLoader;
     private SettingsLoader.Setting masterVolumeSetting;
     protected override void OnEnable()
@@ -18,7 +20,18 @@ public class Settings : SubMenu
 
         MasterVolumeSlider.value = masterVolumeSetting.Get();
 
+        // This seems cursed but may I present to you the Unity docs: https://docs.unity3d.com/2018.3/Documentation/ScriptReference/UI.Slider-onValueChanged.html
+        MasterVolumeSlider.onValueChanged.AddListener(delegate {MasterVolumeAdjusted(); });
+
+        MasterVolumeAdjusted();
         HookButtons();
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        MasterVolumeSlider.onValueChanged.RemoveAllListeners();
     }
 
     private void HookButtons() {
@@ -30,5 +43,9 @@ public class Settings : SubMenu
 
     private void SaveClick() {
         masterVolumeSetting.Set(MasterVolumeSlider.value);
+    }
+
+    private void MasterVolumeAdjusted() {
+        MasterVolumeText.text = $"{MasterVolumeSlider.value}%";
     }
 }
