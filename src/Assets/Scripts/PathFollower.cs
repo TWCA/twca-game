3fed2d83397197fd83fa1667686b6fe09acf62ed
Wren Durbano <wren.durbano@gmail.com>
2026-03-24 15:54:23 -0600

    add movement acceleration

--- src/Assets/Scripts/PathFollower.cs
+++ src/Assets/Scripts/PathFollower.cs
@@ -4,65 +4,91 @@ using UnityEngine;
 
 public class PathFollower : MonoBehaviour
 {
-    /** Pixels per second. */
-    public float speed = 40f;
+    /** Measured in px/s */
+    public float minSpeed = 50f;
+    /** Measured in px/s */
+    public float maxSpeed = 120f;
+    /** Measured in px/s² */
+    public float acceleration = 50f;
+    /** Measured in px/s² */
+    public float deceleration = 500f;
     
     /** How far ahead of the player do we pathfind to when walking with WASD */
     [Range(5.0f, 100f)] public float walkingLookAheadLength = 25f;
     
     /** How much the WASD system should help us to turn when it is detected. Between zero and one.*/
-    [Range(0.0f, 1.0f)] public float turnAssistStrength = 0.5f;
+    [Range(0.0f, 0.9f)] public float turnAssistStrength = 0.5f;
     
     /** How close we have to be to turning before the WASD system helps us turn easier. Between zero and one. */
-    [Range(0.0f, 1.0f)] public float turnAssistThreshold = 0.1f;
+    [Range(0.0f, 0.5f)] public float turnAssistThreshold = 0.1f;
     
     /** How close we have to be to walking perpendicular to the path with WASD before we just stop moving. Between zero and one. */
-    [Range(0.0f, 1.0f)] public float perpendicularMovementThreshold = 0.1f;
+    [Range(0.0f, 0.5f)] public float perpendicularMovementThreshold = 0.1f;
     
     public event Action DonePathing;
 
+    private float currentSpeed;
+    private bool movedLastFrame;
+    
     private List<int> plannedPath;
     private Vector2 plannedEndPosition = Vector2.zero;
 
+    PathFollower()
+    {
+        currentSpeed = minSpeed;
+    } 
+    
     public void FixedUpdate()
     {
-        if (!IsPathfinding()) return;
-
         PathNetwork net = PathNetwork.Instance;
         bool isFuture = TimeManager.Instance.IsFuture();
 
-        // check if the last section of the path is intact
-        if (!net.AreNodesConnected(plannedPath[^2], plannedPath[^1], isFuture))
-            StopPathfinding(); // last section of path broken, the goal (along this path) is now unreachable
+        if (IsPathfinding())
+        {
+            // check if the last section of the path is intact
+            if (!net.AreNodesConnected(plannedPath[^2], plannedPath[^1], isFuture))
+                StopPathfinding(); // last section of path broken, the goal (along this path) is now unreachable
 
-        // check if the last section of the path is intact
-        if (!net.AreNodesConnected(plannedPath[^2], plannedPath[^1], isFuture))
-            StopPathfinding(); // last section of path broken, the goal (along this path) is now unreachable
+            // check if the last section of the path is intact
+            if (!net.AreNodesConnected(plannedPath[^2], plannedPath[^1], isFuture))
+                StopPathfinding(); // last section of path broken, the goal (along this path) is now unreachable
 
-        // check path is still valid
-        if (!AStarPathfinder.CheckPathStillValid(plannedPath, isFuture))
-            // if invalid try to recalculate path
-            if (!PathfindTo(plannedEndPosition))
-                // if failed stop pathfinding
-                StopPathfinding();
+            // check path is still valid
+            if (!AStarPathfinder.CheckPathStillValid(plannedPath, isFuture))
+                // if invalid try to recalculate path
+                if (!PathfindTo(plannedEndPosition))
+                    // if failed stop pathfinding
+                    StopPathfinding();
+        }
     }
 
     public void Update()
     {
-        if (!IsPathfinding()) return;
-
-        Vector2 targetPosition = GetPathfindingTarget();
-        float stepSize = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, stepSize);
-
-        if (transform.position.Equals(targetPosition))
+        if (IsPathfinding())
         {
-            plannedPath.RemoveAt(0);
+            Vector2 targetPosition = GetPathfindingNextPointTowardsGoal();
+            float stepSize = currentSpeed * Time.deltaTime;
+            
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, stepSize);
+            movedLastFrame = true;
 
-            // if there are no more edges to follow, stop pathing
-            if (plannedPath.Count <= 1)
-                StopPathfinding();
+            if (transform.position.Equals(targetPosition))
+            {
+                plannedPath.RemoveAt(0);
+
+                // if there are no more edges to follow, stop pathing
+                if (plannedPath.Count <= 1)
+                    StopPathfinding();
+            }
         }
+        
+        if (movedLastFrame)
+            currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.deltaTime);
+        else
+            currentSpeed = Mathf.MoveTowards(currentSpeed, minSpeed, deceleration * Time.deltaTime);
+        Debug.Log(currentSpeed);
+        
+        movedLastFrame = false;
     }
 
     /**
@@ -101,11 +127,11 @@ public class PathFollower : MonoBehaviour
 
     public Vector2 GetPathfindingDirection()
     {
-        Vector2 targetPosition = GetPathfindingTarget();
+        Vector2 targetPosition = GetPathfindingNextPointTowardsGoal();
         return (targetPosition - (Vector2)transform.position).normalized;
     }
 
-    public Vector2 GetPathfindingTarget()
+    public Vector2 GetPathfindingNextPointTowardsGoal()
     {
         if (plannedPath.Count > 2)
             return PathNetwork.Instance.GetNodePosition(plannedPath[1]);
@@ -122,15 +148,19 @@ public class PathFollower : MonoBehaviour
         StopPathfinding();
 
         Vector2 goalPosition = GetWalkingGoal(targetDirection);
-        float stepSize = speed * delta;
-
+        
+        if (goalPosition.Equals(transform.position)) 
+            return Vector2.zero;
+        
+        float stepSize = currentSpeed * delta;
         Vector2 nextPointTowardsGoal = GetNextPointTowardsGoal(goalPosition, stepSize);
 
         // Draw line from player to goal position
         //Debug.DrawLine(transform.position, goalPosition, Color.green);
 
         transform.position = Vector2.MoveTowards(transform.position, nextPointTowardsGoal, stepSize);
-
+        movedLastFrame = true;
+        
         return (nextPointTowardsGoal - (Vector2)transform.position).normalized;
     }
 
