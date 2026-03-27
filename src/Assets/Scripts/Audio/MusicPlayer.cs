f4e86f74a951e0959e2b19cc6b7514e8bdff8bd4
Wren Durbano <wren.durbano@gmail.com>
2026-03-27 13:17:29 -0600

    add music player everywhere

--- src/Assets/Scripts/Audio/MusicPlayer.cs
+++ src/Assets/Scripts/Audio/MusicPlayer.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    private static MusicPlayer _instance;
+    public static MusicPlayer Instance { get; private set; }
 
-    public static MusicPlayer Instance
-    {
-        get { return _instance; }
-    }
     [SerializeField] private AudioSource musicSource;
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
-        DontDestroyOnLoad(this);
-        _instance = this;
+        if (Instance != null && Instance != this)
+            Destroy(Instance.gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+
+        Instance = this;
     }
 
     public void Play(string FilePath)
