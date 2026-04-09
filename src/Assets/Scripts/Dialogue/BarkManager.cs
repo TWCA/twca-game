2526c3d635a78a0b80b7922b5697457bdf96d9cb
Wren Durbano <wren.durbano@gmail.com>
2026-04-09 14:22:26 -0600

    made bark manager track current level

--- src/Assets/Scripts/Dialogue/BarkManager.cs
+++ src/Assets/Scripts/Dialogue/BarkManager.cs
@@ -5,26 +5,47 @@ using UnityEngine;
 public class BarkManager : MonoBehaviour
 {
     public static BarkManager Instance { get; private set; }
+
+    [SerializeField] private Level CurrentLevel;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
         {
+            // update the current level and kill ourselves because there is already an instance
+            Instance.CurrentLevel = this.CurrentLevel;
             Destroy(this.gameObject);
         }
         else
         {
+            // we are the only one, we become the instance
             DontDestroyOnLoad(gameObject);
             Instance = this;
         }
     }
-    
+
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+    }
+
+    private enum Level
+    {
+        None,
+        RobinsRoom,
+        Kitchen,
+        WalkWithSam,
+        Level1,
+        Level2,
+        Level3,
+        Descent,
+        Level4,
+        Level5,
+        Reunited,
+        Return,
+        Ending
     }
 }
