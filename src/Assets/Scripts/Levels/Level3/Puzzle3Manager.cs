116954e0b6bd54bd74e75a393218e1dc906bdee0
Jacob McCormick <jmmcc52@hotmail.com>
2026-07-17 03:19:28 -0600

    Fixed Fire Audio

--- src/Assets/Scripts/Levels/Level3/Puzzle3Manager.cs
+++ src/Assets/Scripts/Levels/Level3/Puzzle3Manager.cs
@@ -10,18 +10,46 @@ public class Puzzle3Manager : MonoBehaviour
     public GameObject FullBucket;
     public TimePortal TimePortal;
     public ParticleSystem BushFire;
+    public GameObject fire;
+    public AudioSource fireSource;
+    public bool burning = true;
+    public AudioClip PutOutClip;
 
     // Start is called before the first frame update
     void Start()
     {
+        fireSource = fire.GetComponent<AudioSource>();
+
         TimeManager.Instance.onTimeChanged += HandleTimeChanged;
         BurningBush.ItemPlaced += HandleFireExtinguished;
+        fireSource.loop = true;
+        fireSource.Play();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimeManager.Instance.GetLightingTime() >= 0.8)
+        {
+            if (burning)
+            {
+                fireSource.Pause();
+                burning = false;
+                Debug.Log("Pausing burning Audio");
+            }
+        } else {
+            if (fire.GetComponent<ItemDropNode>().used != true)
+            {
+                if (!burning)
+                {
+                    fireSource.Play();
+                    burning = true;
+                    Debug.Log("playing burning audio");
+                }
+            }
+        }
     }
 
     void HandleTimeChanged() {
@@ -36,5 +64,16 @@ public class Puzzle3Manager : MonoBehaviour
 
         pathNetwork.SetPathPastTraversable(pathNetwork.GetNamedPath("bush"), true);
         Destroy(BushFire);
+        fireSource.PlayOneShot(PutOutClip);
+        StartCoroutine(PlayEnd(fireSource));
+        //fireSource.Pause();
+        
+        Debug.Log("Put Out Fire. Ending burning audio.");
+    }
+
+    public static IEnumerator PlayEnd(AudioSource source)
+    {
+        yield return new WaitForSeconds(3f);
+        source.Pause();
     }
 }
